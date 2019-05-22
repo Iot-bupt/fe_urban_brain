a2e149092d280079276519deb75a0de948bd20fe
yellowpig <yellowpig1210@163.com>
2019-05-22 22:16:56 +0800

    汽车icon

--- src/components/digitalCard/index.js
+++ src/components/digitalCard/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './style.css';
 
+import carIcon from "./../../static/image/car.png";
+
 const DigitalCard = (data) => {
   const { title, items } = data
 
@@ -11,7 +13,7 @@ const DigitalCard = (data) => {
         { items.map((item,index) => {
           return (
             <div className="itemContent" key={ index }>
-              <div className="itemIcon"></div>
+              <div><img src={ carIcon } alt="汽车" className="itemIcon"/></div>
               <div className="itemContext">
                 <div className="itemTitle">{ item.name }</div>
                 <div className="itemNumber">{ item.count }</div>
